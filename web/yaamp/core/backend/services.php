6dad2ab40bd9a78be351991d88cc588c0e6e2d08
Petri <petri.hetta@dicode.fi>
2019-09-23 23:16:09 +0300

    update to nicehash API v2

--- web/yaamp/core/backend/services.php
+++ web/yaamp/core/backend/services.php
@@ -1,10 +1,211 @@
 <?php
 
 /* NiceHash Stuff */
+
+// Return X-Auth header for given nicehash request parameters
+function get_nicehash_auth($apikey, $apisecret, $time, $nonce, $organization, $method, $path, $querystring, $body, $json)
+{
+    $data =
+        mb_convert_encoding($apikey, "ISO-8859-1") . "\0" .
+        mb_convert_encoding($time, "ISO-8859-1") . "\0" .
+        mb_convert_encoding($nonce, "ISO-8859-1") . "\0" .
+        "\0" .
+        mb_convert_encoding($organization, "ISO-8859-1") . "\0" .
+        "\0" .
+        mb_convert_encoding($method, "ISO-8859-1") . "\0" .
+        mb_convert_encoding($path, "ISO-8859-1") . "\0" .
+        mb_convert_encoding($querystring, "ISO-8859-1") . "\0";
+    if ($json) {
+        $data .= mb_convert_encoding($body, "UTF-8");
+    } else {
+        $data .= $body;
+    }
+    return hash_hmac("sha256",  $data, $apisecret);
+}
+
+// Build a nicehash API v2 request and execute it, returning result.
+function execute_nicehash_request($url, $method, $body, $public, $json) {
+    if ($public) {
+        // Public requests don't need signing headers
+        $s = curl_init();
+        curl_setopt($s, CURLOPT_URL, "https://api2.nicehash.com" . $url);
+        curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
+        $res = curl_exec($s);
+        curl_close($s);
+        if ($res === false) {
+            return null;
+        }
+        return $res;
+    }
+    $apikey = NICEHASH_API_KEY;
+    $url_ary = explode("?", $url);
+    $path = $url_ary[1];
+    $querystring = $url_ary[2];
+    $apisecret = ""; // TODO add apisecret to config
+    $organization = ""; // TODO add organisation to config
+    $time = round(microtime(true) * 1000);
+    $nonce = gen_uuid();
+    $request_id = gen_uuid(); // Just generate an uuid, this field isn't really applicable here.
+    $auth = get_nicehash_auth($apikey, $apisecret, $time, $nonce, $organization, $method, $path, $querystring, $body, $json);
+
+    $curl_opts = array(
+        "X-Time: " . $time,
+        "X-Nonce: " . $nonce,
+        "X-Organization-Id: " . $organization,
+        "X-Request-Id: " . $request_id,
+        "X-Auth: " . $apikey . ":" . $auth
+    );
+
+    if ($json) {
+        array_push($curl_opts, "Accept: application/json;charset=UTF-8");
+    }
+
+    $s = curl_init();
+    curl_setopt($s, CURLOPT_URL, "https://api2.nicehash.com" . $url);
+    curl_setopt($s, CURLOPT_HTTPHEADER, $curl_opts);
+    curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
+    $res = curl_exec($s);
+    curl_close($s);
+    if ($res === false) {
+        return null;
+    }
+    return $res;
+}
+
+// Turn a decimal number to API v2 compliant string value
+function nicehash_num2str($num) {
+    return number_format($num, 8, ".", "");
+}
+
+function nicehash_account($currency) {
+    return execute_nicehash_request(
+        "/main/api/v2/accounting/account/" . $currency, "GET",
+        "",
+        false, false
+    );
+}
+
+function nicehash_stats_global_current() {
+    return execute_nicehash_request(
+        "/main/api/v2/public/stats/global/current/", "GET",
+        "",
+        true, false
+    );
+}
+
+function nicehash_order_remove($id) {
+    return execute_nicehash_request(
+        "/main/api/v2/hashpower/order/" . $id, "DELETE",
+        "",
+        false, false
+    );
+}
+
+function nicehash_orders_get_my($algo) {
+    return execute_nicehash_request(
+        "/main/api/v2/hashpower/myOrders?algo=" . $algo . "&limit=1", "GET",
+        "",
+        false, false
+    );
+}
+
+function nicehash_order_create($type, $limit, $poolId, $price, $marketFactor, $displayMarketFactor, $amount, $algorithm, $market) {
+    $body = array(
+        "type" => $type,
+        "limit" => nicehash_num2str($limit),
+        "poolId" => $poolId,
+        "price" => nicehash_num2str($price),
+        "marketFactor" => nicehash_num2str($marketFactor),
+        "displayMarketFactor" => $displayMarketFactor,
+        "amount" => nicehash_num2str($amount),
+        "algorithm" => $algorithm,
+        "market" => $market
+    );
+    return execute_nicehash_request(
+        "/main/api/v2/hashpower/order", "POST",
+        json_encode($body),
+        false, true
+    );
+}
+
+function nicehash_order_update_price_and_limit($order, $price, $limit) {
+    $body = array(
+        "marketFactor" => nicehash_num2str($order->marketFactor),
+        "displayMarketFactor" => nicehash_num2str($order->displayMarketFactor),
+        "limit" => nicehash_num2str($limit),
+        "price" => nicehash_num2str($price)
+    );
+    return execute_nicehash_request(
+        "/main/api/v2/hashpower/order/" . $order->id . "/updatePriceAndLimit","POST",
+        json_encode($body),
+        false, true
+    );
+}
+
+function nicehash_order_refill($id, $amount) {
+    $body = array(
+        "amount" => nicehash_num2str($amount)
+    );
+    return execute_nicehash_request(
+        "/main/api/v2/hashpower/order/" . $id . "/refill", "POST",
+        json_encode($body),
+        false, true
+    );
+}
+
+function nicehash_get_pools() {
+    return execute_nicehash_request(
+        "/main/api/v2/pools/", "GET",
+        "",
+        false, false
+    );
+}
+
+function nicehash_create_pool($status, $host, $port, $username, $password, $algo, $name) {
+    $body = array(
+        "status" => $status,
+        "password" => $password,
+        "username" => $username,
+        "stratumPort" => $port,
+        "stratumHostname" => $host,
+        "algorithm" => $algo,
+        "name" => $name
+    );
+    return execute_nicehash_request(
+        "/main/api/v2/pool/", "POST",
+        json_encode($body),
+        false, true
+    );
+}
+
+// Returns pool ID for specified pool data. Creates pool for organization if it doesn't already exist.
+// This is required because API v2 removed support for directly providing pool parameters.
+function nicehash_get_pool_id($host, $port, $username, $password, $algorithm) {
+    // Check if a pool already exists
+    $res = nicehash_get_pools();
+    if (!$res) {
+        throw new Exception("Failed to request pools");
+    }
+    $data = json_decode($res);
+    foreach ($data->list as $pool) {
+        if ($pool->host == $host && $pool->port == $port && $pool->username == $username && $pool->password == $password && $pool->algorithm == $algorithm) {
+            // Found a pool matching parameters
+            return $pool->id;
+        }
+    }
+
+    // No pool found, create it
+    $res = nicehash_create_pool("VERIFIED", $host, $port, $username, $password, $algorithm, "YAAMP pool");
+    if (!$res) {
+        throw new Exception("Failed to create pool");
+    }
+    $data = json_decode($res);
+    return $data->id;
+}
+
 function BackendUpdateServices()
 {
 //	debuglog(__FUNCTION__);
-
 	if (YAAMP_USE_NICEHASH_API != true)
 		return;
 
@@ -41,7 +242,7 @@ function BackendUpdateServices()
 		29=>'skunk',
 	);
 
-	$res = fetch_url('https://api.nicehash.com/api?method=stats.global.current');
+	$res = nicehash_stats_global_current();
 	if(!$res) return;
 
 	$a = json_decode($res);
@@ -102,17 +303,14 @@ function BackendUpdateServices()
 	if (YAAMP_USE_NICEHASH_API != true)
 		return;
 
-	$apikey = NICEHASH_API_KEY;
-	$apiid = NICEHASH_API_ID;
-
 	$deposit = NICEHASH_DEPOSIT;
 	$amount = NICEHASH_DEPOSIT_AMOUNT;
 
-	$res = fetch_url("https://api.nicehash.com/api?method=balance&id=$apiid&key=$apikey");
+	$res = nicehash_account("BTC");
 	debuglog($res);
 
 	$a = json_decode($res);
-	$balance = $a->result->balance_confirmed;
+	$balance = $a->balance;
 
 	foreach($table as $i=>$algo)
 	{
@@ -128,7 +326,7 @@ function BackendUpdateServices()
 		{
 			if($nicehash->orderid)
 			{
-				$res = fetch_url("https://api.nicehash.com/api?method=orders.remove&id=$apiid&key=$apikey&location=0&algo=$i&order=$nicehash->orderid");
+			    $res = nicehash_order_remove($nicehash->orderId);
 				debuglog($res);
 
 				$nicehash->orderid = null;
@@ -149,16 +347,17 @@ function BackendUpdateServices()
 		$maxprice = $price*0.9;
 		$cancelprice = $price*1.1;
 
-		$res = fetch_url("https://api.nicehash.com/api?method=orders.get&my&id=$apiid&key=$apikey&location=0&algo=$i");
+		$res = nicehash_orders_get_my($algo);
 		if(!$res) break;
 
 		$a = json_decode($res);
-		if(count($a->result->orders) == 0)
+		if(count($a->list) == 0)
 		{
 			if($balance < $amount) continue;
 			$port = getAlgoPort($algo);
 
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.create&id=$apiid&key=$apikey&location=0&algo=$i&amount=$amount&price=$setprice&limit=0&pool_host=yaamp.com&pool_port=$port&pool_user=$deposit&pool_pass=xx");
+			$poolId = nicehash_get_pool_id("yaamp.com", $port, $deposit, "xx", $algo);
+			$res = nicehash_order_create("STANDARD", 0, $poolId, $setprice, 100000, 100000, $amount, $algo, "EU");
 			debuglog($res);
 
 			$nicehash->last_decrease = time();
@@ -167,29 +366,28 @@ function BackendUpdateServices()
 			continue;
 		}
 
-		$order = $a->result->orders[0];
+		$order = $a->list[0];
 		debuglog("$algo $order->price $minprice $setprice $maxprice $cancelprice");
 
 		$nicehash->orderid = $order->id;
-		$nicehash->btc = $order->btc_avail;
-		$nicehash->workers = $order->workers;
+		$nicehash->btc = $order->availableAmount;
+		$nicehash->workers = $order->rigsCount;
 		$nicehash->price = $order->price;
-		$nicehash->speed = $order->limit_speed;
-		$nicehash->accepted = $order->accepted_speed;
+		$nicehash->speed = $order->limit;
+		$nicehash->accepted = $order->acceptedCurrentSpeed;
 
-		if($order->price > $cancelprice && $order->workers > 0)
+		if($order->price > $cancelprice && $order->rigsCount > 0)
 		{
 			debuglog("* cancel order $algo");
 
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.remove&id=$apiid&key=$apikey&location=0&algo=$i&order=$order->id");
+			$res = nicehash_order_remove($order->id);
 			debuglog($res);
 		}
 
-		else if($order->price > $maxprice && $order->limit_speed == 0)
+		else if($order->price > $maxprice && $order->limit == 0)
 		{
 			debuglog("* decrease speed $algo");
-
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.set.limit&id=$apiid&key=$apikey&location=0&algo=$i&order=$order->id&limit=0.05");
+            $res = nicehash_order_update_price_and_limit($order, $order->price, 0.05);
 			debuglog($res);
 		}
 
@@ -197,33 +395,30 @@ function BackendUpdateServices()
 		{
 			debuglog("* decrease price $algo");
 
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.set.price.decrease&id=$apiid&key=$apikey&location=0&algo=$i&order=$order->id");
+			nicehash_order_update_price_and_limit($order, $maxprice, $order->limit);
 			debuglog($res);
 
 			$nicehash->last_decrease = time();
 		}
 
-		else if($order->price < $minprice && $order->workers <= 0)
+		else if($order->price < $minprice && $order->rigsCount <= 0)
 		{
 			debuglog("* increase price $algo");
-
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.set.price&id=$apiid&key=$apikey&algo=$i&location=0&order=$order->id&price=$setprice");
+            nicehash_order_update_price_and_limit($order, $setprice, $order->limit);
 			debuglog($res);
 		}
 
-		else if($order->price < $maxprice && $order->limit_speed == 0.05)
+		else if($order->price < $maxprice && $order->limit == 0.05)
 		{
 			debuglog("* increase speed $algo");
-
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.set.limit&id=$apiid&key=$apikey&location=0&algo=$i&order=$order->id&limit=0");
+			nicehash_order_update_price_and_limit($order, $order->price, 0);
 			debuglog($res);
 		}
 
-		else if($order->btc_avail < 0.00075000)
+		else if($order->availableAmount < 0.00075000)
 		{
 			debuglog("* refilling order $order->id");
-
-			$res = fetch_url("https://api.nicehash.com/api?method=orders.refill&id=$apiid&key=$apikey&location=0&algo=$i&order=$order->id&amount=0.01");
+            $res = nicehash_order_refill($order->id, 0.01);
 			debuglog($res);
 		}
 
@@ -231,11 +426,3 @@ function BackendUpdateServices()
 	}
 
 }
-
-
-
-
-
-
-
-
